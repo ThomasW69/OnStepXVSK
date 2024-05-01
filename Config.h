f2039cba35fc24b057f0ac6d85fae23c3f9ccf64
ThomasW69 <ThomasW69@users.noreply.github.com>
2024-05-01 16:34:20 +0200

    Goto Update

--- Config.h
+++ Config.h
@@ -26,10 +26,16 @@
 #define SERIAL_A_BAUD_DEFAULT       57600 //   9600, n. Where n=9600,19200,57600,115200,230400,460800 (common baud rates.)    Infreq
 #define SERIAL_B_BAUD_DEFAULT       57600 //   9600, n. Baud rate as above. See (src/pinmaps/) for Serial port assignments.   Infreq
 #define SERIAL_B_ESP_FLASHING         OFF //    OFF, ON Upload ESP8266 WiFi firmware through SERIAL_B with :ESPFLASH# cmd.    Option
+<<<<<<< Updated upstream
 #define SERIAL_C_BAUD_DEFAULT         OFF //    OFF, n. Baud rate as above. See (src/pinmaps/) for Serial port assignments.   Infreq
 #define SERIAL_D_BAUD_DEFAULT         OFF //    OFF, n. Baud rate as above. See (src/pinmaps/) for Serial port assignments.   Infreq
 #define SERIAL_E_BAUD_DEFAULT         OFF //    OFF, n. Baud rate as above. See (src/pinmaps/) for Serial port assignments.   Infreq
 #define SERIAL_RADIO                  OFF //    OFF, Use BLUETOOTH or WIFI_ACCESS_POINT or WIFI_STATION (ESP32 only.)         Option
+=======
+#define SERIAL_C_BAUD_DEFAULT       57600 //    OFF, n. Baud rate as above. See (src/pinmaps/) for Serial port assignments.   Infreq
+#define SERIAL_D_BAUD_DEFAULT       OFF //    OFF, n. Baud rate as above. See (src/pinmaps/) for Serial port assignments.   Infreq
+#define SERIAL_E_BAUD_DEFAULT       OFF //    OFF, n. Baud rate as above. See (src/pinmaps/) for Serial port assignments.   Infreq
+>>>>>>> Stashed changes
 
 // STATUS --------------------------------------------- see https://onstep.groups.io/g/main/wiki/Configuration_Controller#STATUS_LED
 #define STATUS_LED                    OFF //    OFF, Steady illumination if no error, blinks w/error code otherwise.          Option
@@ -60,6 +66,25 @@
 #ifndef Kuppel
 
   //ab hier der 30er
+  // MOUNT -------------------------------------------------------- see https://onstep.groups.io/g/main/wiki/Configuration_Mount#MOUNT
+  #define MOUNT_TYPE                GEM_TA //    GEM, GEM         German Equatorial Mount, etc. that need meridian flips.     <-Req'd
+                                          //         GEM_TA      GEM w/tangent arm Declination
+                                          //         GEM_TAC     GEM w/tangent arm Declination and geometry correction
+                                          //         FORK        Fork Mount
+                                          //         FORK_TA     FORK w/tangent arm Declination
+                                          //         FORK_TAC    FORK w/tangent arm Declination and geometry correction
+                                          //         ALTAZM      Altitude/Azimuth Mount, Dobsonians, etc.
+                                          //         ALTAZM_UNL  ALTAZM w/unlimited Azimuth motion
+
+
+  #define MOUNT_COORDS          TOPOCENTRIC // ...RIC, Applies refraction to coordinates to/from OnStep, except exactly         Infreq
+                                            //              at the poles. Use TOPO_STRICT to apply refraction even in that case.
+                                            //              Use OBSERVED_PLACE for no refraction.
+
+  #define MOUNT_ENABLE_IN_STANDBY       OFF //    OFF, ON Enables mount motor drivers while in standby.                         Infreq
+ 
+
+
   // AXIS1 RA/AZM -------------------------------------------------------- see https://onstep.groups.io/g/main/wiki/Configuration_Axes
   #define AXIS1_DRIVER_MODEL        TMC2209 //    OFF, Enter motor driver model (above) in both axes to activate the mount.    <-Often
 
@@ -134,6 +159,10 @@
   #define AXIS2_SENSE_LIMIT_MAX LIMIT_SENSE // ...NSE, HIGH or LOW state on limit sense switch stops movement.                  Option
   #define AXIS2_HOME_DEFAULT             7  // angle in degrees, for the Dec or Altitude Axis.
   #define AXIS2_SENSE_HOME_OFFSET        0   // offset in arc-seconds to home from the sense position
+  #define AXIS2_SLEW_RATE_PERCENT        50.0F //Axis 2 langsamermachen für Homing
+
+
+#else  //ab hier die Kuppel
 
   // MOUNT -------------------------------------------------------- see https://onstep.groups.io/g/main/wiki/Configuration_Mount#MOUNT
   #define MOUNT_TYPE                GEM_TA //    GEM, GEM         German Equatorial Mount, etc. that need meridian flips.     <-Req'd
@@ -147,13 +176,10 @@
 
 
   #define MOUNT_COORDS          TOPOCENTRIC // ...RIC, Applies refraction to coordinates to/from OnStep, except exactly         Infreq
-                                            //              at the poles. Use TOPO_STRICT to apply refraction even in that case.
-                                            //              Use OBSERVED_PLACE for no refraction.
+                                          //              at the poles. Use TOPO_STRICT to apply refraction even in that case.
+                                          //              Use OBSERVED_PLACE for no refraction.
 
   #define MOUNT_ENABLE_IN_STANDBY       OFF //    OFF, ON Enables mount motor drivers while in standby.                         Infreq
-
-#else  //ab hier die Kuppel
-
 
   // AXIS1 RA/AZM -------------------------------------------------------- see https://onstep.groups.io/g/main/wiki/Configuration_Axes
   #define AXIS1_DRIVER_MODEL        TMC2209 //    OFF, Enter motor driver model (above) in both axes to activate the mount.    <-Often
@@ -229,6 +255,7 @@
   #define AXIS2_SENSE_HOME_OFFSET        0   // offset in arc-seconds to home from the sense position
   #define AXIS2_HOME_DEFAULT             7 // angle in degrees, for the Dec or Altitude Axis.
 
+<<<<<<< Updated upstream
   // MOUNT -------------------------------------------------------- see https://onstep.groups.io/g/main/wiki/Configuration_Mount#MOUNT
   #define MOUNT_TYPE                GEM_TA //    GEM, GEM         German Equatorial Mount, etc. that need meridian flips.     <-Req'd
                                           //         GEM_TA      GEM w/tangent arm Declination
@@ -248,6 +275,8 @@
                                           //         Requires FRAM NV memory and the mount must not move while powered down.
 
 #define MOUNT_ENABLE_IN_STANDBY       OFF //    OFF, ON Enables mount motor drivers while in standby.                         Infreq
+=======
+>>>>>>> Stashed changes
 
 #endif
 
@@ -264,7 +293,11 @@
 #define GPS_MIN_WAIT_MINUTES 0;
 
 // NON-VOLATILE MEMORY --------------------------------------------- see https://onstep.groups.io/g/main/wiki/Configuration_Mount#NV
+<<<<<<< Updated upstream
 #define NV_DRIVER              NV_DEF // NV_DEF, Use platforms default non-volatile device to remember runtime settings.  Option
+=======
+#define NV_DRIVER                 NV_DEF // NV_DEF, Use platforms default non-volatile device to remember runtime settings.  Option
+>>>>>>> Stashed changes
 
 // STATUS ------------------------------------------------------ see https://onstep.groups.io/g/main/wiki/Configuration_Mount#STATUS
 #define STATUS_MOUNT_LED              ON //    OFF, ON Flashes proportional to rate of movement or solid on for slews.       Option
